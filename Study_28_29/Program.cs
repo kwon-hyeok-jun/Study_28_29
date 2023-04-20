693ea2947f929575569c90e8b56e08178803e4f1
혁준 권 <kkynks0@naver.com>
2023-04-20 19:37:05 +0900

    초기 커밋.

---
+++ Study_28_29/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+class EnumerableDemo
+{
+    static void Main()
+    {
+        var numbers = Enumerable.Range(1, 5);
+        foreach (var n in numbers)
+        {
+            Console.Write("{0}\t",n);
+        }
+        Console.WriteLine();
+
+        var sameNumbers = Enumerable.Repeat(-1, 5);
+        foreach (var n in sameNumbers)
+        {
+            Console.Write("{0}\t",n);
+        }
+        Console.WriteLine();
+
+
+        //======================================================
+        int num;
+        List<string> list;
+
+        list = null;
+
+        num = list?.Count ?? 0;
+        Console.WriteLine(num);
+
+        list = new List<string>();
+        list.Add("hi");
+
+        num = list?.Count ?? 0;
+        Console.WriteLine(num);
+
+    }
+}
